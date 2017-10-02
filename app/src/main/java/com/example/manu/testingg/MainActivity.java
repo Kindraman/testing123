c170aca5cb7a849af286e9b185aa320d49aaa164
Manu <manujamming@gmail.com>
2017-10-02 12:59:07 -0300

    Primer up

---
+++ app/src/main/java/com/example/manu/testingg/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.manu.testingg;
+
+import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String msg = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, msg);
+        this.startActivity(intent);
+    }
+}
